b54aa2464621f37475fa326983284c738c5527e9
maggness <timderoller@gmail.com>
2022-02-28 12:24:36 +0100

    gesplit

---
+++ HetKunstAppie/js/imageCarousel.js
@@ -0,0 +1,29 @@
+export function zoekenOpenen() {
+    searchField.classList.toggle("uitklappen");
+  }
+  
+  function leftImages() {
+    const KunstStukDing = document.querySelectorAll(".kunstStukSection");
+    translateXSchilderij = translateXSchilderij + 200;
+    if (translateXSchilderij > 0) {
+      translateXSchilderij = -800;
+    }
+    KunstStukDing.forEach((item) => {
+      item.style.transform = "translateX(" + translateXSchilderij + "vh)";
+    });
+  }
+  
+  function rightImages() {
+    const KunstStukDing = document.querySelectorAll(".kunstStukSection");
+    translateXSchilderij = translateXSchilderij - 200;
+    if (translateXSchilderij < -800) {
+      translateXSchilderij = 0;
+    }
+    KunstStukDing.forEach((item) => {
+      item.style.transform = "translateX(" + translateXSchilderij + "vh)";
+    });
+  }
+  
+  searchIcon.addEventListener("click", zoekenOpenen);
+  leftButton.addEventListener("click", leftImages);
+  rightButton.addEventListener("click", rightImages);
